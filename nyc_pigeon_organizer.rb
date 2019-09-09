e03d8127a9eb95879df424a0b160dd5c8d8c9eb4
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 17:14:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final = {}
+
+  data.each do |color_gender_lives, values|
+    values.each do |specifics, names|
+      names.each do |val|
+        if final[val] == NIL
+          final[val] = {}
+          final[val][color_gender_lives] = []
+        else
+          final[val][color_gender_lives] = []
+        end
+      end
+    end
+  end
+  final.each do |val, new_info|
+    new_info.each do |info_called, specified|
+      data.each do |color_gender_lives, values|
+        values.each do |value, array|
+          array.each do |elements|
+            if elements == val && info_called == color_gender_lives
+              final[val][info_called] << value.to_s 
+            end
+          end
+        end
+      end
+    end
+  end
+ final                                                      #RETURN THE FINAL HASH
 end
