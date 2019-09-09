845231e3d4ccfcab6282792356dd0e390463cd93
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:08:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeon_organized = {}
+  final = {}
   
-  data[:gender].map do |key, value|
-    id = key[i]
-    i = 0 
-    
-    binding.pry
-  end
-    
-    
-  
-  pigeon_organized
+  binding.pry
 end
