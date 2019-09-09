0d7c3def2bfd99edbe63f9476043db24821548d7
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 17:12:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,6 +15,18 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-
+  final.each do |name, new_info|
+    new_info.each do |info_called, array|
+      data.each do |color_gender_lives, values|
+        values.each do |value, array|
+          array.each do |elements|
+            if elements == name && info_called == color_gender_lives
+              final[name][info_called] << value.to_s 
+            end
+          end
+        end
+      end
+    end
+  end
  final                                                      #RETURN THE FINAL HASH
 end
