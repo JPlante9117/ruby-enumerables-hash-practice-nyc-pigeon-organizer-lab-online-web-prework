f8ad163ef80cdfb308bf691c8d05e885897ca3c5
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:45:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,9 @@ def nyc_pigeon_organizer(data)
   final.each do |name, new_info|                            #call on the new hash, pieces being the key (name) and value (color/gender                                                               /lives)
     new_info.each do |info_called, array|                   #iterate into the values, returning the name of the value and it's actual value
       data.each do |color_gender_lives, values|
-        binding.pry
+        values.each do |a, b|
+          binding.pry
+        end
       end
     end
   end
