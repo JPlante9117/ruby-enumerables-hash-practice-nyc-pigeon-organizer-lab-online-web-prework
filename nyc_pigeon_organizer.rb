8d97303eef77eb3bce12067e88941b67a9fb5d91
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:57:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final = {}
 
-  data.each do |color_gender_lives, b|
-    binding.pry
+  data.each do |color_gender_lives, values|
+    values.each do |a, b|
+      binding.pry
+    end
   end
 
  final                                                      #RETURN THE FINAL HASH
