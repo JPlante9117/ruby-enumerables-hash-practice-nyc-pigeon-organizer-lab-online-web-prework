5c80a7716b839125908309641680ddc3fab15a3d
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:59:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,18 @@ def nyc_pigeon_organizer(data)
   final = {}
 
   data.each do |color_gender_lives, values|
-    values.each do |specifics, b|
-      binding.pry
+    values.each do |specifics, names|
+      names.each do |val|
+        if final[val] == NIL
+          final[val] = {}
+          final[val][color_gender_lives] = []
+        else
+          final[val][color_gender_lives] = []
+      end
     end
   end
 
  final                                                      #RETURN THE FINAL HASH
 end
+
+
