677e2495b37bd63ff2b2394dc51a34e8165782db
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:39:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,16 +5,21 @@ def nyc_pigeon_organizer(data)
   
   data.each do |color_gender_lives, values|                 #iterate over the first set of hashes/values from data . . .
     values.each do |value, array|                           #iterate through the second . . .
-      binding.pry
       array.each do |name|                                  #iterate through the third/final . . .
         if final[name] == NIL                               #if there is no current value for the name . . .
-          final[name] = {}                                  #create a new hash with it's value,
-          final[name][color_gender_lives] = []              #and also update it with the information inside.
+          final[name] = {}                                  #create a new hash as it's key,
+          final[name][color_gender_lives] = []              #and also update it with the information inside. (:color => nil, . . . )
         else
-          final[name][color_gender_lives] = []               #Otherwise associate the new value . . .
+          final[name][color_gender_lives] = []              #Otherwise update with the information inside. (:color => nil, . . .)
         end
       end
     end
   end
- final
+  final.each do |name, color_gender_lives|
+    binding.pry
+  end
+  
+  
+  
+ final                                                      #RETURN THE FINAL HASH
 end
