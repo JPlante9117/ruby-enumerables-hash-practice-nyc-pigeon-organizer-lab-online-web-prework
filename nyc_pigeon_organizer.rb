e85007fb8d67601f209188b3e05a5743441ca82a
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:18:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,15 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final = {}
   
-  data.each do |colo_gender_lives, values|
-    binding.pry
+  data.each do |color_gender_lives, values|                 #iterate over the first set of hashes/values . . .
+    values.each do |value, array|                           #iterate through the second . . .
+      array.each do |name|                                  #iterate through the third/final . . .
+        if hash[name] == NIL                                #if there is no current value for the name . . .
+          hash[name] = {}                                   #create a new hash with it's value,
+          hash[name][color_gender_lives_gender_lives] = []  #and also update it with the information inside.
+        end
+      end
+    end
   end
   
   binding.pry
