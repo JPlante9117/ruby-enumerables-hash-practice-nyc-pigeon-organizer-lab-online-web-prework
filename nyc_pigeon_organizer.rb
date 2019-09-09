50cecb12cf8ef135286490e68e371f8ab4dd4a19
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-09 16:29:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,10 +10,24 @@ def nyc_pigeon_organizer(data)
         if final[name] == NIL                               #if there is no current value for the name . . .
           final[name] = {}                                  #create a new hash with it's value,
           final[name][color_gender_lives] = []              #and also update it with the information inside.
+        else
+          hash[name][color_gender_lives] = []               #Otherwise associate the new value . . .
         end
       end
     end
   end
-  
+  final.each do |name, values|                              #Now, look into the data for final . . .
+    values.each do |hashvalue, array|                       #and into it's values . . .
+      data.each do |color_gender_lives, values|             #and into the data values . . .
+        values each do |value, array|
+          array.each do |element|                           #to match the array value 
+            if element == name && hashvalue == color_gender_lives     #if the 
+              final[name][hashvalue] << value.to_s 
+            end
+          end
+        end
+      end
+    end
+  end
  p final
 end
